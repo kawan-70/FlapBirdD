3eeb932d701de0538460cbe3e02b39409d5feda3
flapbir <kauan220656@gmail.com>
2024-10-11 12:44:01 -0300

    qwertyui

--- FlapBirDD/novojogo.xaml.cs
+++ FlapBirDD/novojogo.xaml.cs
@@ -2,42 +2,63 @@ namespace FlapBirDD;
 
 public partial class novojogo : ContentPage
 {
-const int Gravidade=5;
-const int TempoEntreFrames=40;
-bool Imorreu=true;
-double LarguraJanela=0;
-double AlturaJanela=0;
-int velocidade=20;
-void iinicializar()
-{
+	const int gravidade = 3;
+	const int tempoEntreFrames = 25;
+	bool morto = true;
+	double larguraJanela = 0;
+	double alturaJanela = 0;
+	int velocidade = 20;
 
-}
 
-    public novojogo()
+	public novojogo()
 	{
 		InitializeComponent();
 	}
-	void AplicaGravidade()
-	{
-		viao.TranslationY +=Gravidade;
-	}
-	
 
-     async Task Desenhar()
-	 {
-       while (!Imorreu)
-       {
-		AplicaGravidade();
-		await Task.Delay(TempoEntreFrames);
-	   }
-	 }
-   
-	//void  OnGameOverClicked(object s, TappedEventArgs a)
-	//{
-	//	toque.IsVisible = false;
-	//	inicializar();
-	//	Desenhar();
-	//}
+	async Task Desenhar()
+	{
+		while (!morto)
+		{
+			AplicarGravidade();
+			await Task.Delay(tempoEntreFrames);
+			GerenciaCanos();
+		}
+	}
+	void AplicarGravidade()
+	{
+		viao.TranslationY += gravidade;
+	}
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+		larguraJanela=width;
+		alturaJanela=height;
+    }
+
+	void GerenciaCanos ()
+	{
+		canoD1.TranslationX-= velocidade;
+		canoD2.TranslationX-= velocidade;
+		if (canoD2.TranslationX<-larguraJanela)
+		{
+			canoD2.TranslationX=100;
+			canoD1.TranslationX=100;
+		}
+	}
+	void Inicializar()
+	{
+		morto=false;
+	    viao.TranslationY=0;
+	}
+
+	void OnGameOverClicked(object s, TappedEventArgs a)
+	{
+		frameGameOver.IsVisible=false;
+		Inicializar();
+		Desenhar();
+
+	}
+
 
 }
-
