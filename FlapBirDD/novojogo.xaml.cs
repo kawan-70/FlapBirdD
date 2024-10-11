c72ffb99b83f9277277f8a4caddf5fdeb097bc4f
Flapfalha <kauan220656@gmail.com>
2024-10-11 11:06:38 -0300

    qwertyu

--- FlapBirDD/novojogo.xaml.cs
+++ FlapBirDD/novojogo.xaml.cs
@@ -2,10 +2,42 @@ namespace FlapBirDD;
 
 public partial class novojogo : ContentPage
 {
-	
-	public novojogo()
+const int Gravidade=5;
+const int TempoEntreFrames=40;
+bool Imorreu=true;
+double LarguraJanela=0;
+double AlturaJanela=0;
+int velocidade=20;
+void iinicializar()
+{
+
+}
+
+    public novojogo()
 	{
 		InitializeComponent();
 	}
+	void AplicaGravidade()
+	{
+		viao.TranslationY +=Gravidade;
+	}
+	
+
+     async Task Desenhar()
+	 {
+       while (!Imorreu)
+       {
+		AplicaGravidade();
+		await Task.Delay(TempoEntreFrames);
+	   }
+	 }
+   
+	//void  OnGameOverClicked(object s, TappedEventArgs a)
+	//{
+	//	toque.IsVisible = false;
+	//	inicializar();
+	//	Desenhar();
+	//}
+
 }
 
