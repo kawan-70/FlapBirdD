0c5a0224536329c2c02c101e324c31f1d3a742cb
falpBirDD <kauan220656@gmail.com>
2024-10-15 10:41:47 -0300

    q   wertyu

--- FlapBirDD/novojogo.xaml.cs
+++ FlapBirDD/novojogo.xaml.cs
@@ -22,6 +22,13 @@ public partial class novojogo : ContentPage
 			AplicarGravidade();
 			await Task.Delay(tempoEntreFrames);
 			GerenciaCanos();
+			if (VerificaColisao())
+			{
+				morto = true;
+				frameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(tempoEntreFrames);
 		}
 	}
 	void AplicarGravidade()
@@ -29,36 +36,67 @@ public partial class novojogo : ContentPage
 		viao.TranslationY += gravidade;
 	}
 
-    protected override void OnSizeAllocated(double width, double height)
-    {
-        base.OnSizeAllocated(width, height);
-		larguraJanela=width;
-		alturaJanela=height;
-    }
-
-	void GerenciaCanos ()
+	protected override void OnSizeAllocated(double width, double height)
 	{
-		canoD1.TranslationX-= velocidade;
-		canoD2.TranslationX-= velocidade;
-		if (canoD2.TranslationX<-larguraJanela)
+		base.OnSizeAllocated(width, height);
+		larguraJanela = width;
+		alturaJanela = height;
+	}
+
+	void GerenciaCanos()
+	{
+		canod1.TranslationX -= velocidade;
+		canod2.TranslationX -= velocidade;
+		if (canod2.TranslationX < -larguraJanela)
 		{
-			canoD2.TranslationX=100;
-			canoD1.TranslationX=100;
+			canod2.TranslationX = 100;
+			canod1.TranslationX = 100;
 		}
 	}
 	void Inicializar()
 	{
-		morto=false;
-	    viao.TranslationY=0;
+		morto = false;
+		viao.TranslationY = 0;
 	}
 
 	void OnGameOverClicked(object s, TappedEventArgs a)
 	{
-		frameGameOver.IsVisible=false;
+		frameGameOver.IsVisible = false;
 		Inicializar();
 		Desenhar();
 
 	}
 
+	bool VerificaColisao()
+	{
+		if (!morto)
+		{
+			if (VerificaColisaoTeto() ||
+				VerificaColisaoChao())
+			{
+				return true;
+			}
+				
+		}
+		return false;
+	}
+	 private bool VerificaColisaoTeto()
+	{
+		var minY =-alturaJanela/2;
+		if (viao.TranslationY <=minY)
+
+	 	    return true;
+	    else
+			return false;
+	}
+
+	bool VerificaColisaoChao()
+	{
+		var mixY = alturaJanela/2 - chaoo.HeightRequest;
+		if (viao.TranslationY >=mixY)
+	 		return true;
+	 else
+			return false;
+	}
 
 }
